7a1ebfbf223705f91de17c387af4b5263fd71b62
Andrew Myshok <amm982@live.com>
2023-01-05 10:34:24 -0500

    Adding pause menu

--- Assets/Scripts/UI/UIInput.cs
+++ Assets/Scripts/UI/UIInput.cs
@@ -1,25 +1,31 @@
 using UnityEngine;
+using fightInput;
 
 namespace UI
 {
     public class UIInput : MonoBehaviour
     {
-        public static UIInput input;
+        public static UIInput staticInstance;
         [SerializeField] GameObject escapeMenu;
+
         private void Awake()
         {
-            if (input == null)
-                input = this;
+            if (staticInstance == null)
+                staticInstance = this;
             else
                 Destroy(this);
+
+            escapeMenu.SetActive(false);
         }
 
         void Update() 
         {
-             if(Input.GetKeyUp(KeyCode.Escape))
-             {
-                escapeMenu.SetActive(!escapeMenu.activeInHierarchy);
-             }
+            if(Input.GetKeyUp(KeyCode.Escape))
+            {
+                bool toggle = !escapeMenu.activeInHierarchy;
+                escapeMenu.SetActive(toggle);
+                PlayerTurnInputManager.staticInstance.isEnabled = toggle;  
+            }
         }
     }
 }
