ae66e03c43d993ff6c6258904342f96a5f9ac187
Andrew Myshok <amm982@live.com>
2023-05-03 20:08:39 -0400

    added seperate CardConfig class for default values for all cards

--- Assets/Scripts/Fight/Mana/Mana.cs
+++ Assets/Scripts/Fight/Mana/Mana.cs
@@ -1,39 +1,11 @@
 using UnityEngine;
-using System.Collections;
 
 namespace mana
 {
-    public class Mana : MonoBehaviour
+    [CreateAssetMenu(fileName = "Mana", menuName = "Mana/New Mana")]
+    public class Mana : ScriptableObject
     {
-        public ManaType manaType;
-        bool rotating = true;
-        Vector3 DEFAULT_SCALE = new Vector3(.4f,.4f,.4f);
-        void Start() 
-        {
-            this.transform.localScale = DEFAULT_SCALE;
-            StartCoroutine(RotateSelf());
-        }
-        public void ResetScale()
-        {
-            this.transform.localScale = DEFAULT_SCALE;
-        }
-        IEnumerator RotateSelf()
-        {
-            while(rotating)
-            {
-                this.transform.Rotate(.1f, .1f, .1f, Space.Self);
-                yield return null;
-            }
-        }
-    }
-
-    public enum ManaType
-    {
-        Red,
-        Blue,
-        Green,
-        White,
-        Gold,
-        Black
+        public ManaType type;
+        
     }
 }
