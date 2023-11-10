dd91e7f8458ed2463cc1aecd2b8ddc8eecb5181a
Andrew Myshok <amm982@live.com>
2023-11-10 11:36:12 -0500

    Added menuhandle

---
+++ Assets/Scripts/UI/Menus/Common/IMenuHandle.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+namespace UI.Menus.Common
+{
+    /// <summary>
+    /// Used to reference the gameObject of a menu.
+    /// </summary>
+    public interface IMenuHandle
+    {
+        // ReSharper disable once InconsistentNaming
+        // Needs to be same name as a monobehaviour gameObject property
+        GameObject gameObject { get; }
+    }
+}
