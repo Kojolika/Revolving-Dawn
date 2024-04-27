4da8ee48e3544a989af2925bdd45bc3701f8f38a
Kojolika <amm982@live.com>
2024-04-27 17:11:04 -0400

    Fix display interface drawer to not duplicate if nested DisplayInterfaceDrawer

--- Assets/Scripts/Utils/Editor/DisplayInterfaceDrawer.cs
+++ Assets/Scripts/Utils/Editor/DisplayInterfaceDrawer.cs
@@ -1,6 +1,8 @@
 using System;
 using System.Collections.Generic;
+using System.Collections;
 using System.Linq;
+using System.Reflection;
 using System.Text.RegularExpressions;
 using ModestTree;
 using Tooling.Logging;
@@ -50,15 +52,15 @@ namespace Data.Utils.Editor
                 defaultSelection = defaultDisplayType.Name;
             }
 
-            MyLogger.Log($"defualt value : {defaultSelection}. current type :{currentType}");
             var dropdown = new DropdownField(label, derivedTypes.Select(type => type.Name).ToList(), defaultSelection);
+            StyleDropdown(dropdown);
             root.Add(dropdown);
 
             var typeDisplay = new VisualElement();
-            StyleElement(typeDisplay);
+            StyleInterfaceElement(root);
             DisplayConcreteType(defaultDisplayType, root, typeDisplay, property);
 
-            dropdown.RegisterValueChangedCallback((ChangeEvent<string> evt) =>
+            dropdown.RegisterCallback((ChangeEvent<string> evt) =>
             {
                 var newType = derivedTypes
                     .Where(type => type.Name == evt.newValue)
@@ -72,7 +74,6 @@ namespace Data.Utils.Editor
 
         public void DisplayConcreteType(Type type, VisualElement root, VisualElement displayContainer, SerializedProperty property)
         {
-
             if (root.Contains(displayContainer))
             {
                 displayContainer.Clear();
@@ -90,14 +91,16 @@ namespace Data.Utils.Editor
                 && serializedProperty.name == propName
                 && serializedProperty.type != $"managedReference<{type.Name}>")
             {
-                MyLogger.Log($"Setting obj value, old type {serializedProperty.type}, new type: {type}");
                 serializedProperty.serializedObject.Update();
                 serializedProperty.managedReferenceValue = Activator.CreateInstance(type);
                 serializedProperty.serializedObject.ApplyModifiedProperties();
             }
 
             var endProperty = serializedProperty.GetEndProperty();
-            var visitedProperties = new HashSet<uint>();
+
+            // Start iterating upon the first managedReference
+            serializedProperty.Next(true);
+
             do
             {
                 // Only display the property once
@@ -113,23 +116,88 @@ namespace Data.Utils.Editor
                     && !serializedProperty.propertyPath.Contains("m_FileID")
                     && !serializedProperty.propertyPath.Contains($"m_PathID"))
                 {
-                    MyLogger.Log($"Displaying {serializedProperty.propertyPath}");
                     var propertyField = new PropertyField(serializedProperty);
                     propertyField.BindProperty(serializedProperty);
                     displayContainer.Add(propertyField);
                 }
             }
-            while (serializedProperty.Next(true));
+            while (serializedProperty.Next(serializedProperty.propertyType != SerializedPropertyType.ManagedReference || !DoesPropertyHaveAttribute(serializedProperty)));
 
             root.Add(displayContainer);
         }
 
-        void StyleElement(VisualElement typeDisplay)
+        /// <summary>
+        /// Checks to see a property has a <see cref="DisplayInterfaceAttribute"/>.
+        /// </summary>
+        /// <param name="property">Property to check.</param>
+        /// <returns>True if the property has a <see cref="DisplayInterfaceAttribute"/></returns>
+        bool DoesPropertyHaveAttribute(SerializedProperty property)
+        {
+            var targetObjectType = property.serializedObject.targetObject.GetType();
+            string[] objectPropertyNames = property.propertyPath.Split('.');
+            for (int i = 0; i < objectPropertyNames.Length; i++)
+            {
+                string propName = objectPropertyNames[i];
+
+                if (i + 1 < objectPropertyNames.Length && objectPropertyNames[i + 1] == "Array"
+                    && i + 2 < objectPropertyNames.Length && objectPropertyNames[i + 2].StartsWith("data["))
+                {
+                    i += 2;
+                }
+
+                var fieldInfo = targetObjectType.GetField(propName, BindingFlags.NonPublic | BindingFlags.Public | BindingFlags.Instance);
+
+                if (fieldInfo != null)
+                {
+                    if (fieldInfo.FieldType.IsInterface && fieldInfo.GetCustomAttribute<DisplayInterfaceAttribute>() != null)
+                    {
+                        return true;
+                    }
+
+                    // Check if object is array, get array type
+                    targetObjectType = fieldInfo.FieldType.GetElementType() ?? fieldInfo.FieldType;
+
+                    // otherwise if the object is an IEnumerable, find the type of IEnumerable
+                    if (typeof(IEnumerable).IsAssignableFrom(targetObjectType))
+                    {
+                        if (targetObjectType.IsGenericType() && typeof(IEnumerable<>) == targetObjectType.GetGenericTypeDefinition())
+                        {
+                            targetObjectType = targetObjectType.GetGenericArguments()[0];
+                        }
+                        else
+                        {
+                            foreach (var iType in targetObjectType.GetInterfaces())
+                            {
+                                if (iType.IsGenericType() && typeof(IEnumerable<>) == iType.GetGenericTypeDefinition())
+                                {
+                                    targetObjectType = iType.GetGenericArguments()[0];
+                                }
+                            }
+                        }
+                    }
+                }
+                else
+                {
+                    return false;
+                }
+            }
+
+            return false;
+        }
+
+        void StyleDropdown(VisualElement dropdown)
+        {
+            dropdown.style.borderBottomColor = Color.white;
+            dropdown.style.borderBottomWidth = 1;
+            dropdown.style.marginBottom = 5;
+        }
+
+        void StyleInterfaceElement(VisualElement typeDisplay)
         {
             typeDisplay.style.color = Color.black;
             typeDisplay.style.marginLeft = 30;
 
-            var padding = 10;
+            var padding = 5;
             typeDisplay.style.paddingBottom = padding;
             typeDisplay.style.paddingLeft = padding;
             typeDisplay.style.paddingRight = padding;
