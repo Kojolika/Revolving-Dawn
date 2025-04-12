f93d7abbbf186eccb61777b9f630b47778d5946e
Kojolika <amm982@live.com>
2025-04-12 18:34:28 -0400

    Fleshing out ByteCodeInterpreter.cs

--- Assets/Scripts/Fight/Engine/ByteCodeInterpreter.cs
+++ Assets/Scripts/Fight/Engine/ByteCodeInterpreter.cs
@@ -1,4 +1,5 @@
 using System;
+using UnityEngine.Assertions;
 using System.Collections.Generic;
 using Tooling.Logging;
 
@@ -13,11 +14,19 @@ namespace Fight.Engine
 
         // LITERALS
         LiteralBool,
-        LiteralByte,
         LiteralInt,
         LiteralLong,
         LiteralFloat,
         LiteralDouble,
+
+        /// <summary>
+        /// The input format is as follows for arrays.
+        ///  1. byte indicating array instruction (<see cref="LiteralArray"/>
+        ///  2. byte indicating type of array (i.e. <see cref="LiteralBool"/>, <see cref="LiteralInt"/> etc.)
+        ///  3. 2 bytes indicating length of array
+        ///  4. bytes that correspond to the array values, this will vary depending on the size of the array type and length
+        /// </summary>
+        LiteralArray,
 
         // OPERATORS
         OperatorAnd,
@@ -50,73 +59,112 @@ namespace Fight.Engine
             Null,
 
             /// <summary>
-            /// This value will be 0 or 1.
+            /// This value will be 0 (false) or 1 (true).
             /// </summary>
             Bool,
 
-            Byte,
             Int,
             Long,
             Float,
-            Double
+            Double,
+            Array
+        }
+
+
+        /// <summary>
+        /// Used to store <see cref="ValueType.Null"/>, <see cref="ValueType.Bool"/>, <see cref="ValueType.Byte"/>, <see cref="ValueType.Int"/>
+        /// and the length of the array for <see cref="ValueType.Array"/>
+        /// </summary>
+        public readonly long LongValue;
+
+        /// <summary>
+        /// Used to store <see cref="ValueType.Float"/> and <see cref="ValueType.Double"/>
+        /// </summary>
+        public readonly double DoubleValue;
+
+        /// <summary>
+        /// The type of value on the stack
+        /// </summary>
+        public readonly ValueType Type;
+
+        /// <summary>
+        /// If <see cref="Type"/> is an array, this is the value type that the array holds.
+        /// </summary>
+        public readonly ValueType ArrayValueType;
+
+        /// <summary>
+        /// The byte values of the array.
+        /// </summary>
+        public readonly byte[] ArrayValues;
+
+        /// <summary>
+        /// Standard constructor for all value types.
+        /// </summary>
+        private StackValue(long longValue, double doubleValue, ValueType type, ValueType arrayValueType = ValueType.Null)
+        {
+            LongValue = longValue;
+            DoubleValue = doubleValue;
+            Type = type;
+            ArrayValueType = arrayValueType;
+            ArrayValues = null;
         }
 
         /// <summary>
-        /// Used to store <see cref="ValueType.Null"/>, <see cref="ValueType.Bool"/>, <see cref="ValueType.Byte"/>
-        /// and <see cref="ValueType.Int"/>
+        /// Array constructor
         /// </summary>
-        public readonly int IntValue;
-
-        public readonly long LongValue;
-        public readonly float FloatValue;
-        public readonly double DoubleValue;
-        public readonly ValueType Type;
-
-        private StackValue(int intValue, long longValue, float floatValue, double doubleValue, ValueType type)
+        /// <param name="arrayValueType">The type of value of the array</param>
+        /// <param name="length">The length of the array</param>
+        /// <param name="values">The array values</param>
+        private StackValue(ValueType arrayValueType, int length, byte[] values)
         {
-            IntValue = intValue;
-            LongValue = longValue;
-            FloatValue = floatValue;
-            DoubleValue = doubleValue;
-            Type = type;
+            ArrayValueType = arrayValueType;
+            LongValue = length;
+            ArrayValues = values;
+            Type = ValueType.Array;
+
+            DoubleValue = 0;
         }
+
+        #region Helpers
 
         public static StackValue CreateBool(bool value)
         {
-            return new StackValue(value ? 1 : 0, 0, 0, 0, ValueType.Bool);
-        }
-
-        public static StackValue CreateByte(byte value)
-        {
-            return new StackValue(value, 0, 0, 0, ValueType.Byte);
+            return new StackValue(value ? 1 : 0, 0, ValueType.Bool);
         }
 
         public static StackValue CreateInt(int value)
         {
-            return new StackValue(value, 0, 0, 0, ValueType.Int);
+            return new StackValue(value, 0, ValueType.Int);
         }
 
         public static StackValue CreateLong(long value)
         {
-            return new StackValue(0, value, 0, 0, ValueType.Long);
+            return new StackValue(value, 0, ValueType.Long);
         }
 
         public static StackValue CreateFloat(float value)
         {
-            return new StackValue(0, 0, value, 0, ValueType.Float);
+            return new StackValue(0, value, ValueType.Float);
         }
 
         public static StackValue CreateDouble(double value)
         {
-            return new StackValue(0, 0, 0, value, ValueType.Double);
+            return new StackValue(0, value, ValueType.Double);
         }
+
+        public static StackValue CreateArray(ValueType arrayValueType, int length, byte[] values)
+        {
+            return new StackValue(arrayValueType, length, values);
+        }
+
+        #endregion
     }
 
     public class ByteCodeInterpreter
     {
-        public static void Interpret(List<byte> instructions)
+        public static void Interpret(byte[] instructions)
         {
-            var instructionCount = instructions.Count;
+            var instructionCount = instructions.Length;
             int index = 0;
 
             var stack = new List<StackValue>();
@@ -129,59 +177,105 @@ namespace Fight.Engine
                     case InstructionType.Null:
                         MyLogger.LogError($"Invalid instruction at index: {index}!");
                         break;
+
                     case InstructionType.LiteralBool:
-                        Push(
-                            StackValue.CreateBool(
-                                BitConverter.ToBoolean(new[] { instructions[++index] })
-                            )
-                        );
+                        Push(StackValue.CreateBool(
+                            BitConverter.ToBoolean(new[] { instructions[++index] })));
                         break;
-                    case InstructionType.LiteralByte:
-                        Push(
-                            StackValue.CreateByte(instructions[++index])
-                        );
-                        break;
+
                     case InstructionType.LiteralInt:
-                        Push(
-                            StackValue.CreateInt(
-                                BitConverter.ToInt32(new[] { instructions[++index], instructions[++index] })
-                            )
-                        );
+                        Push(StackValue.CreateInt(
+                            BitConverter.ToInt32(new[] { instructions[++index], instructions[++index] })));
                         break;
+
                     case InstructionType.LiteralLong:
-                        Push(
-                            StackValue.CreateLong(
-                                BitConverter.ToInt64(new[]
-                                {
-                                    instructions[++index], instructions[++index], instructions[++index], instructions[++index]
-                                })
-                            )
-                        );
+                        Push(StackValue.CreateLong(BitConverter.ToInt64(new[]
+                        {
+                            instructions[++index], instructions[++index], instructions[++index], instructions[++index]
+                        })));
                         break;
+
                     case InstructionType.LiteralFloat:
-                        Push(
-                            StackValue.CreateFloat(
-                                BitConverter.ToSingle(new[] { instructions[++index], instructions[++index] })
-                            )
-                        );
+                        Push(StackValue.CreateFloat(
+                            BitConverter.ToSingle(new[] { instructions[++index], instructions[++index] })));
                         break;
+
                     case InstructionType.LiteralDouble:
-                        Push(
-                            StackValue.CreateDouble(
-                                BitConverter.ToDouble(new[]
-                                {
-                                    instructions[++index], instructions[++index], instructions[++index], instructions[++index]
-                                })
-                            )
-                        );
+                        Push(StackValue.CreateDouble(BitConverter.ToDouble(new[]
+                        {
+                            instructions[++index], instructions[++index], instructions[++index], instructions[++index]
+                        })));
                         break;
+
+                    case InstructionType.LiteralArray:
+                        // TODO: array support?
+                        var stackValueType = GetStackValueTypeFromInstruction((InstructionType)instructions[++index]);
+                        var valueByteSize = GetNumberOfBytesForValue(stackValueType);
+                        var arrayLength = BitConverter.ToInt32(new[] { instructions[++index], instructions[++index] });
+                        var byteArray = new byte[arrayLength * valueByteSize];
+
+                        for (var i = 0; i < byteArray.Length; i++)
+                        {
+                            byteArray[i] = instructions[++index];
+                        }
+
+                        Push(StackValue.CreateArray(stackValueType, arrayLength, byteArray));
+                        break;
+
                     case InstructionType.OperatorAnd:
-                        
+                        var val1 = Pop();
+                        Assert.IsTrue(val1.Type == StackValue.ValueType.Bool);
+                        var val2 = Pop();
+                        Assert.IsTrue(val2.Type == StackValue.ValueType.Bool);
+                        var and = val1.LongValue & val2.LongValue;
+
+                        Push(StackValue.CreateBool(and != 0));
                         break;
+
                     case InstructionType.OperatorOr:
+                        val1 = Pop();
+                        Assert.IsTrue(val1.Type == StackValue.ValueType.Bool);
+                        val2 = Pop();
+                        Assert.IsTrue(val2.Type == StackValue.ValueType.Bool);
+                        var or = val1.LongValue | val2.LongValue;
+
+                        Push(StackValue.CreateBool(or != 0));
                         break;
+
                     case InstructionType.OperatorAdd:
+                        val1 = Pop();
+                        Assert.IsTrue(val1.Type is StackValue.ValueType.Int
+                            or StackValue.ValueType.Long
+                            or StackValue.ValueType.Float
+                            or StackValue.ValueType.Double);
+
+                        val2 = Pop();
+                        Assert.IsTrue(val2.Type is StackValue.ValueType.Int
+                            or StackValue.ValueType.Long
+                            or StackValue.ValueType.Float
+                            or StackValue.ValueType.Double);
+
+                        var addedResult = (val1.Type is StackValue.ValueType.Int or StackValue.ValueType.Long
+                                              ? val1.LongValue
+                                              : val1.DoubleValue)
+                                          + (val2.Type is StackValue.ValueType.Int or StackValue.ValueType.Long
+                                              ? val2.LongValue
+                                              : val2.DoubleValue);
+                        var type = GetTypeForMathOperation(val1.Type, val2.Type);
+                        var stackValue = type switch
+                        {
+                            StackValue.ValueType.Int => StackValue.CreateInt((int)addedResult),
+                            StackValue.ValueType.Long => StackValue.CreateLong((long)addedResult),
+                            StackValue.ValueType.Float => StackValue.CreateFloat((float)addedResult),
+                            StackValue.ValueType.Double => StackValue.CreateDouble(addedResult),
+                            _ => default
+                        };
+
+                        Assert.IsTrue(stackValue.Type != StackValue.ValueType.Null);
+
+                        Push(stackValue);
                         break;
+
                     case InstructionType.OperatorSubtract:
                         break;
                     case InstructionType.OperatorMultiply:
@@ -196,8 +290,13 @@ namespace Fight.Engine
                         break;
                     case InstructionType.GetAllCombatParticipants:
                         break;
+                    case InstructionType.GetStat:
+                    case InstructionType.SetStat:
+                    case InstructionType.GetBuff:
+                    case InstructionType.SetBuff:
                     default:
-                        throw new ArgumentOutOfRangeException();
+                        MyLogger.LogError($"Invalid instruction at index: {index}! Passed instruction: {instructions[index]}!");
+                        break;
                 }
 
                 index++;
@@ -223,6 +322,63 @@ namespace Fight.Engine
                 stackIndex--;
                 return value;
             }
+        }
+
+        private static StackValue.ValueType GetStackValueTypeFromInstruction(InstructionType instructionType)
+        {
+            return instructionType switch
+            {
+                InstructionType.LiteralBool => StackValue.ValueType.Bool,
+                InstructionType.LiteralInt => StackValue.ValueType.Int,
+                InstructionType.LiteralLong => StackValue.ValueType.Long,
+                InstructionType.LiteralFloat => StackValue.ValueType.Float,
+                InstructionType.LiteralDouble => StackValue.ValueType.Double,
+                InstructionType.LiteralArray => StackValue.ValueType.Array,
+                _ => StackValue.ValueType.Null
+            };
+        }
+
+        private static int GetNumberOfBytesForValue(StackValue.ValueType valueType)
+        {
+            return valueType switch
+            {
+                StackValue.ValueType.Bool => sizeof(bool),
+                StackValue.ValueType.Int => sizeof(int),
+                StackValue.ValueType.Long => sizeof(long),
+                StackValue.ValueType.Float => sizeof(float),
+                StackValue.ValueType.Double => sizeof(double),
+                _ => 0
+            };
+        }
+
+        private static StackValue.ValueType GetTypeForMathOperation(StackValue.ValueType operandType1, StackValue.ValueType operandType2)
+        {
+            if (operandType1 == operandType2)
+            {
+                return operandType1;
+            }
+
+            if (operandType1 is StackValue.ValueType.Double || operandType2 is StackValue.ValueType.Double)
+            {
+                return StackValue.ValueType.Double;
+            }
+
+            switch (operandType1)
+            {
+                case StackValue.ValueType.Int when operandType2 is StackValue.ValueType.Long:
+                    return StackValue.ValueType.Long;
+                case StackValue.ValueType.Long when operandType2 is StackValue.ValueType.Int:
+                    return StackValue.ValueType.Long;
+
+                case StackValue.ValueType.Int when operandType2 is StackValue.ValueType.Float:
+                case StackValue.ValueType.Long when operandType2 is StackValue.ValueType.Float:
+                    return StackValue.ValueType.Float;
+                case StackValue.ValueType.Float when operandType2 is StackValue.ValueType.Int:
+                case StackValue.ValueType.Float when operandType2 is StackValue.ValueType.Long:
+                    return StackValue.ValueType.Float;
+            }
+
+            return StackValue.ValueType.Null;
         }
     }
 }
