20d83c53480697d3e588d138f45bcf6761ce6e6f
Kojolika <amm982@live.com>
2025-05-24 17:25:12 -0400

    unused

--- Assets/Scripts/Tooling/StaticData/EditorUI/IconPaths.cs
+++ Assets/Scripts/Tooling/StaticData/EditorUI/IconPaths.cs
@@ -9,6 +9,5 @@ namespace Tooling.StaticData
         public static readonly string Edit = "Assets/Sprites/Editor/icon_edit.png";
         public static readonly string List = "Assets/Sprites/Editor/icon_list.png";
         public static readonly string Circle = "Assets/Sprites/Editor/icon_circle.png";
-        public static readonly string Add = "Assets/Sprites/Editor/icon_add.png";
     }
 }
